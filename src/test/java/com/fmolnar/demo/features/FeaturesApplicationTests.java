f3122aa301f7b4cedcab65fba19ce1434094b238
Ferenc Molnar <molnarferi90@gmail.com>
2025-05-06 17:58:53 +0200

    Init Repo Technical features

---
+++ src/test/java/com/fmolnar/demo/features/FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmolnar.demo.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
